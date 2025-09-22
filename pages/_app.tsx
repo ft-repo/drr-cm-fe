871c089981353429ed28a7ca888ab6c29341edf8
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2025-09-22 09:31:17 +0700

    fix: applied antd patch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import '@ant-design/v5-patch-for-react-19';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
