b21d7fc48bf334a85b8c1caa4030b08c30ff6c27
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2025-10-02 20:49:34 +0700

    fix: added image

--- components/layout/Sider.tsx
+++ components/layout/Sider.tsx
@@ -1,4 +1,4 @@
-import { Menu, MenuItemProps } from 'antd'
+import { Menu } from 'antd'
 import React, { useCallback, useMemo } from 'react'
 import menu from '@/menu';
 import Image from 'next/image';
@@ -22,7 +22,8 @@ import {
   FaPaperPlane,
   FaPaperclip,
   FaDatabase,
-  FaPlug
+  FaPlug,
+  FaImage
 } from "react-icons/fa6";
 import { useAppSelector } from '@/lib/hooks';
 import { useRouter } from 'next/router';
@@ -49,14 +50,15 @@ const ICON_LIST: any = {
   FaPaperPlane,
   FaPaperclip,
   FaDatabase,
-  FaPlug
+  FaPlug,
+  FaImage
 }
 
 const Side: React.FC<Props> = (props) => {
   const { } = props
   const { role } = useAppSelector(state => state.auth.credential)
   const router = useRouter()
-  const currentTab = menu[role]?.find(item => item.pathname === router.pathname)
+  const currentTab = menu[role as keyof typeof menu]?.find(item => item.pathname === router.pathname)
 
   const Icon = useCallback((iconName: any, { ...props }) => {
     const IconResult = ICON_LIST[iconName]
@@ -70,12 +72,17 @@ const Side: React.FC<Props> = (props) => {
   const renderMenu = useMemo(() => {
     if (!menu[role as keyof typeof menu]) return []
 
-    return menu[role].map((item: MenuItemProps) => {
+    return menu[role as keyof typeof menu].map((item) => {
       if (!item.children) {
         return {
           ...item,
           icon: Icon(item.icon, {}),
-          onClick: (e) => router.push(menu[role].find((item: MenuItemProps) => item.key === e.key).pathname)
+          onClick: (e: { key: string; }) => {
+            const pathname = menu[role as keyof typeof menu]?.find(item => item.key === e.key)?.pathname
+            if (pathname) {
+              router.push(pathname)
+            }
+          }
         }
       }
       // RETURN
